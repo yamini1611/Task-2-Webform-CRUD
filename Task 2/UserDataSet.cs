c856a15c0ae470adf95b6a445996694a939f63ca
yamini1611 <yaminipriya1611@gmail.com>
2024-03-19 12:54:04 +0530

    Revert "commiti changes"
    This reverts commit 0bfa55124a43beb8df3a85eb9e8a0520e0c6ee04.

---
+++ Task 2/UserDataSet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Task_2
+{
+    partial class UserDataSet
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["WebFormsTrainingConnectionString"].ConnectionString;
+
+        // CRUD operation to insert a new user
+        public void InsertUser(string userName, string email)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("InsertUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@UserName", userName);
+                command.Parameters.AddWithValue("@Email", email);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // CRUD operation to select all users
+        public DataTable SelectUser()
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("SelectUsers", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }
+
+            return dataTable;
+        }
+
+        // CRUD operation to update an existing user
+        public void UpdateUser(int userID, string userName, string email)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("UpdateUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@UserID", userID);
+                command.Parameters.AddWithValue("@UserName", userName);
+                command.Parameters.AddWithValue("@Email", email);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // CRUD operation to delete a user
+        public void DeleteUser(int userID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("DeleteUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@UserID", userID);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
